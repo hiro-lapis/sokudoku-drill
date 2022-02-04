e85b646ff6b66503de66f3cf2e1c8ba55ef6be7a
hiro-lapis <coolish555ddddd@gmail.com>
2022-02-05 01:48:21 +0900

    ランダムシャッフルの実現

--- pages/slide-word.vue
+++ pages/slide-word.vue
@@ -3,10 +3,16 @@
     <p>slide_word</p>
     <input v-model="count">
     <button @click="change">ワードチェンジ</button>
-    <!-- count数 -->
-    <template v-for="(word, key) in wordList">
-        <p :key="key">{{word}}</p>
-    </template>
+    <div class="container">
+      <!-- count数 -->
+      <template v-for="(list, index) in wordList">
+        <div :key="index" class="c-word-list">
+          <template v-for="(word, key) in list">
+            <p :key="key" class="c-word">{{ word }}</p>
+          </template>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -30,25 +36,35 @@ export default Vue.extend({
   methods:{
     change() {
       this.wordList = [];
-      const list = [
-        '男は西の山へ',
-        '女は東の山へ',
-        '男は西の川へ',
-        '女は西の川へ',
-        '男は東の川へ',
-        '女は東の川へ',
-        '女は西の山へ',
-      ];
 
-      for (let index = 1; index < this.count; index++) {
-        const item = this.shuffle(list);
-        this.wordList.push(item);
+        const list = [
+          '男は西の山へ',
+          '女は東の山へ',
+          '男は西の川へ',
+          '女は西の川へ',
+          '男は東の川へ',
+          '女は東の川へ',
+          '女は西の山へ',
+        ];
+      for (let index = 0; index < this.count; index++) {
+        this.wordList.push(this.shuffle(list).concat());
+        console.log(this.wordList);
       }
     },
-    shuffle(list: string[]) {
+    shuffle(list: string[]): string[] {
       return list.sort(() => 0.5 - Math.random());
     }
   },
 
 })
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+.c-word-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
