1e7aaac91503e5766fb3247df3d94a4185ab583b
hiro-lapis <coolish555ddddd@gmail.com>
2022-02-06 01:31:41 +0900

    reset.css設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default defineNuxtConfig ({
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    // reset.css
+    '~/assets/css/reset'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
