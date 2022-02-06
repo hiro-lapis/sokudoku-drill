8d0ab57fd2b61cb2406834a6c4faf24ef7d22e67
hiro-lapis <coolish555ddddd@gmail.com>
2022-02-06 15:27:32 +0900

    スライドワードページへの導線追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
-<div>
-  <NuxtLink to="/typing">Typing page</NuxtLink>
-  <NuxtLink to="/slide-word">スライドワード</NuxtLink>
-</div>
+  <div>
+    <NuxtLink to="/slide-word">スライドワード</NuxtLink>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
 })
 </script>
