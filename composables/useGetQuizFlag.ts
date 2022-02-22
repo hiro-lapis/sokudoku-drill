d77587798f376b6f897f39caabbf93590c89fbb1
hiro-lapis <coolish555ddddd@gmail.com>
2022-02-22 12:46:20 +0900

    コード調整

--- composables/useGetQuizFlag.ts
+++ composables/useGetQuizFlag.ts
@@ -11,12 +11,12 @@ export const useGetQuizFlag = (level: number) => {
   let basisValue = Math.floor(Math.random() * 100)
 
   // レベル:ふつう/むずかしいの時は29%で最も低い要素を回答する問題を作成
-  worstFlag = basisValue >= 70;
+  worstFlag = basisValue >= 70
 
   // レベル:むずかしいの時は29%でさらに答え[不明[になる問題を作成
   if (level === 2) {
     basisValue = Math.floor(Math.random() * 100)
-    unclearFlag = basisValue >= 70;
+    unclearFlag = basisValue >= 70
   }
   return { worstFlag, unclearFlag }
 }
