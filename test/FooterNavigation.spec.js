3d1ce3acc67e69dccfe0dc58a0e0c0f4de9297df
hiro-lapis <coolish555ddddd@gmail.com>
2022-03-05 15:17:02 +0900

    フッターのテストコードエラー解消

--- test/FooterNavigation.spec.js
+++ test/FooterNavigation.spec.js
@@ -4,15 +4,22 @@ import FooterNavigation from '@/components/FooterNavigation.vue'
 describe('FooterNavigation', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(FooterNavigation, {
+      shallow: true,
       stubs: {
-      NuxtLink: RouterLinkStub,
+        NuxtLink: RouterLinkStub,
+        'font-awesome-icon': { template: '<i></i>' }
       }
     })
     expect(wrapper.vm).toBeTruthy()
-    expect(wrapper.contains('footer')).toBe(true)
+    expect(wrapper.get('footer').selector).toBe('footer')
 
-    const links = wrapper.findAll(RouterLinkStub)
-    // 各リンクテキストのチェック
+    const links = wrapper.findAllComponents(RouterLinkStub)
+    // 各テキスト&リンクのチェック
     expect(links.at(0).text()).toBe('スライドワード')
+    expect(links.at(0).props().to).toBe('/slide-word')
+    expect(links.at(1).text()).toBe('ブロックパターン')
+    expect(links.at(1).props().to).toBe('/block-pattern')
+    expect(links.at(2).text()).toBe('ABCロジック')
+    expect(links.at(2).props().to).toBe('/abc-logic')
   })
 })
