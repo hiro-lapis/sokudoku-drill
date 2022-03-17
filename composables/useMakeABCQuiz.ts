30276d7fa5410be8e8c9c85c0917fa721c6f0d81
hiro-lapis <coolish555ddddd@gmail.com>
2022-03-18 00:04:21 +0900

    ふつうの難易度問題作成

--- composables/useMakeABCQuiz.ts
+++ composables/useMakeABCQuiz.ts
@@ -1,6 +1,6 @@
 import Quiz from '../types/Quiz'
-import common from '@/plugins/common'
 import { getElementList, getElementForHint, isClearQuiz } from './useMakeABCQuiz/function'
+import common from '@/plugins/common'
 
 /**
  * @param boolean worstFlag 最も低い・短い・小さい回答問題作成フラグ
@@ -65,14 +65,56 @@ export const useMakeABCQuiz = (worstFlag: boolean, unclearFlag: boolean): Quiz =
   /** ふつう(最下を問い回答が明確)なクイズを作成 */
   const makeNormalQuiz = (list: string[]): Quiz => {
     // 回答を最初の要素に設定(答えが最下を問うものでも共通)
-    // key[2]が最下位
-    const answer = list[2]
+    // key[0]が最下位
+    const answer = list[0]
     // ヒント問いで使用する比較表現
     const expression =  expressionList[common.getRandomInt(expressionList.length)]
     // 最下を問う文章を設定
     const question = '一番'+ expression.low + 'のは?'
+    const questionIsHigh = false
     // ヒント文で上位比較ワードを使用するかどうかのフラグ
     let isHighExp = common.getRandomBool()
+        // ヒント文1で使用する要素を決定
+    const [element1, element2] = getElementList()
+    const [value1, value2] = [list.indexOf(element1), list.indexOf(element2)]
+    let hint1: string
+    // 要素のkey数を基準に優劣を比較
+    if (isHighExp) {
+      hint1 = (value1 > value2 ? element1 : element2) + 'は' + (value1 > value2 ? element2 : element1) + 'より' + expression.high
+    } else {
+      hint1 = (value1 < value2 ? element1 : element2) + 'は' + (value1 < value2 ? element2 : element1) + 'より' + expression.low
+    }
+
+    // ヒント文2の作成
+    isHighExp = common.getRandomBool()
+    // ヒント文1で使われてない要素はヒント文2で使用必須化
+    const required = list.find(element => element !== element1 && element !==  element2) !
+    let [element3, element4] = getElementForHint(required)
+    let [value3, value4] = [list.indexOf(element3), list.indexOf(element4)]
+    let isClear = isClearQuiz(questionIsHigh, value1, value2, value3, value4)
+
+    // 必ず回答が出せるロジックになるよう調整
+    while (isClear === false) {
+      [element3, element4] = getElementForHint(required)
+      value3 = list.indexOf(element3)
+      value4 = list.indexOf(element4)
+      isClear = isClearQuiz(questionIsHigh, value1, value2, value3, value4)
+    }
+
+    // 要素のkey数を基準に優劣を比較
+    let hint2: string
+    if (isHighExp) {
+      hint2 = (value3 > value4 ? element3 : element4) + 'は' + (value3 > value4 ? element4 : element3) + 'より' + expression.high
+    } else {
+      hint2 = (value3 < value4 ? element3 : element4) + 'は' + (value3 < value4 ? element4 : element3) + 'より' + expression.low
+    }
+    return new Quiz(
+      hint1,
+      hint2,
+      question,
+      answer,
+      list
+    )
   /**
    * TODO:比較ロジック実装
    * 必ず答えを出すためのヒント文生成
@@ -89,10 +131,10 @@ export const useMakeABCQuiz = (worstFlag: boolean, unclearFlag: boolean): Quiz =
   // console.log(unclearFlag)
 
   // let quiz: Quiz;
-  if (!unclearFlag && !worstFlag) {
+  if (!worstFlag && !unclearFlag) {
     return makeEasyQuiz(getElementList());
   }
-  if (unclearFlag && !worstFlag) {
+  if (worstFlag && !unclearFlag) {
     return makeNormalQuiz(getElementList());
   }
   // if (unclearFlag && worstFlag) {
